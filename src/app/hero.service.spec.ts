fe425a0bb1afcb30b6099387a19ec475594051b1
Charline Fazentieux <chafazentieuxpro@gmail.com>
2019-05-07 11:03:19 +0200

    adding rooting module but i create a bug

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HeroService } from './hero.service';
+import { Observable, of } from 'rxjs';
+
+
+describe('HeroService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: HeroService = TestBed.get(HeroService);
+    expect(service).toBeTruthy();
+  });
+});
